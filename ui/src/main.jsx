dfdf9ef55b3cd31ed693b6ccec09c966bc74ef1e
andrewcukierwar <andrew.cukierwar@gmail.com>
2025-07-20 22:33:32 -0400

    Added quickstart.md + added React + Vite to frontend

---
+++ ui/src/main.jsx
@@ -0,0 +1,9 @@
+// ui/src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Hello from Vite + React!</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
